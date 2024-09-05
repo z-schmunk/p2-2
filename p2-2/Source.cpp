381d220fc7831d7ce4d10bee3ebffd7c593d6f5e
z-schmunk <z-schmunk@onu.edu>
2024-09-05 09:16:01 -0400

    Add project files.

---
+++ p2-2/Source.cpp
@@ -0,0 +1,17 @@
+/*
+* FIle:		tempConverter.cpp
+* Author:   Zac Schmunk
+* Date:		9/5/2024
+*
+*Description: 
+*/
+#include <iostream>
+#include<string>
+using namespace std;
+int main(void) {
+	string user_name;
+	cout << "Hello, my name is Hal." << endl;
+	cout << "What is your name?" << endl;
+	cin >> user_name; 
+	cout << "Hello, " << user_name << ". I am glad to meet you!";
+}
